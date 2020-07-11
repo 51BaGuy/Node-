9719067d469ff7be9a7b7344f0204d7f0fc80602
baguy <a66554344@gmail.com>
2020-07-11 22:34:26 +0800

    Non-Blocking操作進行請求回應

--- router.js
+++ router.js
@@ -1,10 +1,13 @@
-function route(handle, pathname) {
+// 相對此前從請求處理程序中取得回傳值，這次取而代之的是直接傳遞response物件。如果沒有對應的請求處理器處理，我們就直接回傳 "404" 錯誤。
+function route(handle, pathname, response) {
   console.log("About to route a request for " + pathname);
   if (typeof handle[pathname] === 'function') {
-    return handle[pathname]();
+    handle[pathname](response);
   } else {
     console.log("No request handler found for " + pathname);
-    return "404 Not found";
+    response.writeHead(404, {"Content-Type": "text/plain"});
+    response.write("404 Not found");
+    response.end();
   }
 }
 
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// 相對此前從route()函數取得回傳值的做法，這次我們將response物件作為第三個參數傳遞給route()函數，並且，我們將onRequest()處理程序中所有有關response的函數調都移除，因為我們希望這部分工作讓route()函數來完成。
 var http = require("http");
 var url = require("url");
 
@@ -6,10 +7,7 @@ function start(route, handle) {
     var pathname = url.parse(request.url).pathname;
     console.log("Request for " + pathname + " received.");
 
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    var content = route(handle, pathname)
-    response.write(content);
-    response.end();
+    route(handle, pathname, response);
   }
 
   http.createServer(onRequest).listen(8888);
